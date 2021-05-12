207983a24f53ac018063daa2486ad3bd73af6665
kuba22z <your@email.com>
2021-05-12 13:17:07 +0200

    Aufgabe 3 finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
         padding-top: 60px;
         padding-left: 50px;
         padding-right: 50px;*/
-        width: 600px;
+        width: 800px;
         margin-left: auto;
         margin-right: auto;
         margin-top: 0;
@@ -43,40 +43,89 @@
 <body>
 <script>
     "use strict";
-    var menu = {
-        'Home': {},
-        'Kategorien': {},
-        'Verkaufen': {},
 
-        'Unternehmen' : {
-            "Philosophie" :{},
-            "Karriere": {}
+    class Menu {
+        menu = {
+            'Home': {},
+            'Kategorien': {},
+            'Verkaufen': {},
+
+            'Unternehmen': {
+                "Philosophie": {},
+                "Karriere": {}
+            }
+        };
+        ul;
+        ulContainer
+
+        constructor() {
+            this.ulContainer = document.createElement("div");
+            this.ulContainer.setAttribute('class','menuDiv');
+            document.body.appendChild(this.ulContainer)
+            this.fillWithTopics();
         }
-    };
+        fillWithTopics(){
+            this.ul  =document.createElement("ul");
+            this.ul.setAttribute('class','menu-outer-ul');
+            this.ulContainer.appendChild(this.ul);
 
+            for (let key in this.menu)
+            {
+                var liOuter = document.createElement("li");
+                liOuter.setAttribute('class','menu-outer-li');
+                this.ul.appendChild(liOuter)
+                liOuter.innerText = key;
+                for(let elements in this.menu[key]){
+                    var ulInner  =document.createElement("ul");
+                    liOuter.appendChild(ulInner)
+                    var liInner = document.createElement("li");
+                    liInner.setAttribute('class','menu-inner-li');
+                    ulInner.appendChild(liInner)
+                    liInner.innerText = elements;
+                }
 
-var div = document.createElement("div");
-div.setAttribute('class','menuDiv');
-document.body.appendChild(div)
-var ul  =document.createElement("ul");
-ul.setAttribute('class','menu-outer-ul');
-div.appendChild(ul);
-for (let key  in menu)
-    {
-        var liOuter = document.createElement("li");
-        liOuter.setAttribute('class','menu-outer-li');
-        ul.appendChild(liOuter)
-        liOuter.innerText = key;
-        for(let elements in menu[key]){
-            var ulInner  =document.createElement("ul");
-            liOuter.appendChild(ulInner)
-            var liInner = document.createElement("li");
-            liInner.setAttribute('class','menu-inner-li');
-            ulInner.appendChild(liInner)
-            liInner.innerText = elements;
+            }
+
         }
+
+        addTopic(newTopic,content){
+            this.menu[newTopic]=content
+        }
+
+        addToTopic(topic,content){
+            if(this.menu[topic]) {
+                //push new subtopic(content) to topic
+                this.menu[topic][content] ={}
+            }
+            else console.log("This topic doesnt exists")
+        }
+
+        removeTopic(topic){
+            if(this.menu[topic]) {
+                delete this.menu[topic];
+
+            }
+            else console.log("This topic doesnt exists")
+        }
+        removeSubTopic(topic,subtopic){
+            if(this.menu[topic][subtopic])
+                delete this.menu[topic][subtopic];
+            else console.log("This topic doesnt exists")
+        }
+
+        updateMenu(){
+        this.ul.remove()
+        this.fillWithTopics();
+        }
+
 
     }
+    let m=new Menu();
+    m.addTopic('About',{'Adresse':{}})
+    m.removeTopic('Home')
+    m.removeSubTopic("Unternehmen","Karriere")
+    m.addToTopic("Unternehmen","Geschichte")
+    m.updateMenu()
 
 </script>
 </body>
